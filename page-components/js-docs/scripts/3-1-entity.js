7415bfa0e8a5fe95d00506385f8631883fdab105
Bangdayeon <dybang00@gmail.com>
2025-08-19 12:35:58 +0900

    3-1 객체 추가

---
+++ page-components/js-docs/scripts/3-1-entity.js
@@ -0,0 +1,84 @@
+const buttonActions = {
+    _1,
+    _2,
+    _3,
+    _4,
+    _5
+};
+Object.entries(buttonActions).forEach(([id, handler]) => {
+    const btn = document.getElementById(id);
+    if (btn) {
+        btn.addEventListener("click", handler);
+    }
+});
+
+function _1() {
+    user = {};      // 1. 빈 객체 user 생성
+    user.name = "John";     // 2. name: John 추가
+    user.surname = "Smith"; // 3. surname: Smith 추가
+    user.name = "Pete";     // 4. name을 Pete로 수정
+    delete user.name;       // 5. name 삭제
+
+    for(let key in user) {
+        if( user.name == undefined && user.surname == "Smith")
+            alert("good");
+    }
+}
+
+function _2() {
+    let pizza = {};
+    let hamburger = {name: "mom's touch"};
+    
+    function isEmpty(obj) {
+        for(let key in obj) {
+            return false;
+        }
+        return true;
+    }
+
+    alert(`Is pizza empty?: ${isEmpty(pizza)} \n Is hamburger empty?: ${isEmpty(hamburger)}`);
+}
+
+function _3() {
+    const user = {
+        name: "John"
+    };
+    let x = user.name = "Pete"; // const는 '객체'에 대해 상수를 선언했으므로 값은 변할 수 있다
+    // let y = user = "Pete";
+
+    alert(`user.name=Pete: ${x}\n user="Pete: undefined`);
+}
+
+function _4() {
+    let salaries = {
+        John: 100,
+        Ann: 160,
+        Pete: 130
+    }
+
+    let sum = 0;
+    for(let key in salaries) {
+        sum += salaries[key];
+    }
+
+    alert(`salaries's sum = ${sum}`);
+}
+
+function _5() {
+    let menu = {
+        width: 200,
+        height: 300,
+        title: "My menu"
+    };
+    let origin_menu = {...menu};
+
+    function multiplyNumeric(obj) {
+        for(key in obj) {
+            if(typeof obj[key] == "number")
+                obj[key] *= 2;
+        }
+    }
+    multiplyNumeric(menu);
+
+    alert(`origin menu: ${JSON.stringify(origin_menu)}\nmutiplyNumeric menu: ${JSON.stringify(menu)}`);
+}
